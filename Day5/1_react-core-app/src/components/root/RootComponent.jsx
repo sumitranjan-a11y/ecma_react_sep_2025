6a2bca6f6f461b9d3bc17369038f3d521a5e9d86
Sumit <sumit.ranjan@indiamart.com>
2025-09-26 15:50:35 +0530

    sync

--- Day5/1_react-core-app/src/components/root/RootComponent.jsx
+++ Day5/1_react-core-app/src/components/root/RootComponent.jsx
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
 import ListRootMine from '../2_assignment_mine/ListRootMine';
-=======
 import ListRoot from '../1_working_with_arrays/ListComponent';
 import CrudAssignment from '../2_assignment/CrudAssignment';
->>>>>>> 4ac0c9241644222f7a56c0cfd21bd01fcb79fa5b
 import ErrorHandler from '../common/ErrorHandler';
 
 const RootComponent = () => {
     return (
-<<<<<<< HEAD
-        <div className='container'>
-                <ListRootMine />
-        </div>
-=======
         <ErrorHandler>
             <div className='container'>
                 {/* <ListRoot /> */}
                 <CrudAssignment />
             </div>
         </ErrorHandler>
->>>>>>> 4ac0c9241644222f7a56c0cfd21bd01fcb79fa5b
     );
 };
 
