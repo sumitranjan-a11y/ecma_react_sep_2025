4674e20b9ec28ef354d857526eb477c09642904d
Sumit <sumit.ranjan@indiamart.com>
2025-09-27 11:27:44 +0530

    Mine changes

---
+++ Day6/react-routing-app/src/components/assignment/AssignmentComponentMine.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AssignmentComponent = () => {
+    return (
+        <div>
+            <h1 className="text-primary">Assignment Component</h1>
+            <h4 className="text-warning">This is a Simple, React Routing Application</h4>
+        </div>
+    );
+};
+
+export default AssignmentComponent;
--- Day6/react-routing-app/src/routes/index.js
+++ Day6/react-routing-app/src/routes/index.js
@@ -2,11 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import HomeComponent from "../components/home/HomeComponent";
 import AboutComponent from "../components/about/AboutComponent";
 import NoMatchComponent from "../components/no-match/NoMatchComponent";
+import AdminComponent from "../components/admin/AdminComponentMine";
+import LoginComponent from "../components/login/LoginComponentMine";
+import AssignmentComponent from "../components/assignment/AssignmentComponentMine";
 
 export default (
     <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/about" element={<AboutComponent />} />
+        <Route path="/admin" element={<AdminComponent />} />
+        <Route path="/login" element={<LoginComponent />} />
+        <Route path="/assignment" element={<AssignmentComponent />} />
         <Route path="*" element={<NoMatchComponent />} />
     </Routes>
 );
